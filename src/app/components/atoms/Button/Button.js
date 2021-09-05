bb712640f4aa709168915664888c01b291876d08
Matt Yates <matt.yates52@gmail.com>
2021-09-05 15:54:11 +0100

    tailwind installed

--- src/app/components/atoms/Button/Button.js
+++ src/app/components/atoms/Button/Button.js
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
-import "./Button.scss";
 
-export default function Button({ text, path, classNames }) {
+export default function Button({ text, path }) {
   return (
-    <Link to={path} className="inline-block bg-yellow-500">
+    <Link
+      to={path}
+      className="inline-block text-lg text-white bg-yellow-500 cursor-pointer uppercase px-4 py-2 no-underline font-orbitron"
+    >
       {text}
     </Link>
   );
